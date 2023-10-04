009ae803a83389c5ee1961731ea2c4c924948184
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2023-10-05 00:35:23 +0300

    Added login response model

---
+++ lib/models/login/login_response_model.dart
@@ -0,0 +1,18 @@
+class LoginResponseModel {
+  int? id;
+  String? token;
+
+  LoginResponseModel({this.id, this.token});
+
+  LoginResponseModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    token = json['token'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['token'] = token;
+    return data;
+  }
+}
